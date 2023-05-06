c8d7e372ca372dd6052e3626ac3c21bea9e25d17
Ali The Boss <alialikhahasl@gmail.com>
2023-05-06 21:29:18 +0430

    first gateway commit

---
+++ src/main/java/com/tehranch/paanogateway/PaanoGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.tehranch.paanogateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaanoGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaanoGatewayApplication.class, args);
+	}
+
+}
